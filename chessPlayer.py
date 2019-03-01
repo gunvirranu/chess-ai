127e3a4bc0eeadfd2bd7002681e76cd738e0ac43
Gunvir Ranu <g1ranu@gmail.com>
2019-03-01 10:29:29 -0500

    Created ability for two program to go against itself

--- chessPlayer.py
+++ chessPlayer.py
@@ -1,5 +1,6 @@
 from Board import Board
 from MoveGen import MoveGen
+from MoveEval import MoveEval
 
 
 class ChessGame:
@@ -11,6 +12,7 @@ class ChessGame:
             self.player = player
         self.board = Board(init)
         self.moveGen = MoveGen(self.board, self.player)
+        self.moveEval = MoveEval(self.moveGen, self.player)
 
     def __str__(self):
         out = '\n'
@@ -33,12 +35,50 @@ init = '- - - - - - - - \
         - - - - - - - - \
         - - - - - - - - '
 
-game = ChessGame(10)
 
-moves = game.moveGen.getAllPlayerMoves()
-for i in moves:
-    game.board.boardArr[i[1]] = 21
+def versus():
+    game1 = ChessGame(10)
+    game2 = ChessGame(20)
+    i = 0
+    while True:
+        print(game1)
+        move1 = game1.moveEval.goodMove()
+        if not move1:
+            break
+        game1.board.makeMoveForce(move1)
+        game2.board.makeMoveForce(move1)
+        print(game1)
+        move2 = game2.moveEval.goodMove()
+        if not move2:
+            break
+        game2.board.makeMoveForce(move2)
+        game1.board.makeMoveForce(move2)
+        i += 1
+    print('Ran out of moves')
+    print(i)
 
-print(game)
 
-print(moves)
+versus()
+
+
+def playHuman():
+    game = ChessGame(20)
+    while True:
+        print(game)
+        while True:
+            try:
+                col1, row1 = input('Which piece to move [x y] -> ').split(' ')
+                col2, row2 = input('Where to place [x y] -> ').split(' ')
+                pos1 = 8*int(row1) + int(col1)
+                pos2 = 8*int(row2) + int(col2)
+                if 0 <= pos1 < 64 and 0 <= pos2 < 64:
+                    move = (pos1, pos2)
+                    if move in game.moveGen.getPiecePseudoLegalMoves(pos1):
+                        break
+            except Exception:
+                pass
+        game.board.makeMoveForce(move)
+        print(game)
+        move = game.moveEval.goodMove()
+        print(move)
+        game.board.makeMoveForce(move)
